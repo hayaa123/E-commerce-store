a39e1c0545b023013fa05e374a3c1a6eb9f10726
hayaa1234 <hayalawansah@gmail.com>
2022-01-30 21:39:06 +0200

    regester on admin site

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,21 @@
+from pyexpat import model
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from  .models import (
+    Category,
+    Product,
+    ProductImage,
+    ProductSpecification,
+    ProductSpecificationValue,
+    ProductType,
+)
+admin.site.register(Category,MPTTModelAdmin)
 
-# Register your models here.
+class ProductSpecificationInline(admin.TabularInline):
+    model = ProductSpecification
+
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductSpecificationInline,
+    ]
