aa244fad82ee91ae30fe8249b96deadd7cc47524
hayaa1234 <hayalawansah@gmail.com>
2022-02-06 21:32:42 +0200

    added image serializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,10 +1,19 @@
 from importlib.metadata import files
+from itertools import product
 from statistics import mode
 from rest_framework import serializers
 
-from .models import Product
+from .models import Product, ProductImage
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ['image','alt_text']
+
 
 class ProductSerializer(serializers.ModelSerializer):
+    product_image = ImageSerializer(many=True , read_only=True)
+
     class Meta :
         model = Product
-        fields = ["id","title","description"]
+        fields = ["id","title","description","regular_price","product_image"]
